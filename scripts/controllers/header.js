034cc190c906ddabfd395cc3e25f0c18bebb5e6a
Roy Evan Sia <roysia2004@gmail.com>
2012-09-30 22:06:45 +0800

    first commit.

---
+++ scripts/controllers/header.js
@@ -0,0 +1,9 @@
+'use strict';
+
+polldApp.controller('HeaderCtrl', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+});
---
+++ scripts/controllers/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+polldApp.controller('MainCtrl', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+});
