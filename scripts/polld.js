aec6ae4e5c33dd19cb532eee7ad9de2d4a6d3566
Roy Evan Sia <roysia2004@gmail.com>
2012-10-04 21:01:13 +0800

    added directives.

--- scripts/polld.js
+++ scripts/polld.js
@@ -21,3 +21,37 @@ var polldApp = angular.module('polldApp', [])
         redirectTo: '/'
       });
   }]);
+
+// Define directives
+polldApp.directive('ngEnter', function() {
+  return function(scope, element, attrs) {
+    element.bind('keyup', function(event) {
+      if (event.keyCode === 13)
+        scope.$apply(attrs.ngEnter);
+    });
+  };
+}).directive('navList', function(){
+  return {
+    restrict: 'E',
+    transclude: true,
+    template: '<nav><ul ng-transclude></ul></nav>',
+    replace: true
+  };
+}).directive('navItem', function($location){
+  return {
+    restrict: 'E',
+    transclude: true,
+    scope: { path: '@' },
+    controller: function($scope, $location) {
+      $scope.selected = false;
+      
+      $scope.$watch(function(){
+        return $location.path();
+      }, function(path){
+        $scope.selected = path === $scope.path;
+      });
+    },
+    template: '<li ng-class="{active:selected}"><a ng-href="#{{path}}" ng-transclude></a></li>',
+    replace: true
+  };
+});
